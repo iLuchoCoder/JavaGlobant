002c03b9c4ed6bfae5805e59352c02565a2d5b44
luis.pineda <luis.pineda@globant.com>
2022-03-03 10:40:40 -0500

    Initial

---
+++ Topic5 - DBMS/databasespring/src/test/java/com/luchoglober/databasespring/DatabasespringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luchoglober.databasespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabasespringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
