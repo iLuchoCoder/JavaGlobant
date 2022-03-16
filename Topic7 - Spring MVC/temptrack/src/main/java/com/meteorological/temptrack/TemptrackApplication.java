e9de0026bb0e17109eb6b02579016be50fe01146
iLuchoGlober88 <luis.pineda@globant.com>
2022-03-16 15:55:46 -0500

    Topic7 - Spring MVC

---
+++ Topic7 - Spring MVC/temptrack/src/main/java/com/meteorological/temptrack/TemptrackApplication.java
@@ -0,0 +1,13 @@
+package com.meteorological.temptrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemptrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemptrackApplication.class, args);
+	}
+
+}
---
+++ Topic7 - Spring MVC/temptrack/src/test/java/com/meteorological/temptrack/TemptrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meteorological.temptrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemptrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
