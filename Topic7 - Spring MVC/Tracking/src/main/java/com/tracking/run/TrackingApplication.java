69ceeaebbab581284b5bdb16326599048ba064af
iLuchoGlober88 <luis.pineda@globant.com>
2022-03-20 00:59:16 -0500

    Topic 7 - Spring MVC

--- Topic7 - Spring MVC/Tracking/src/main/java/com/tracking/run/TrackingApplication.java
+++ Topic7 - Spring MVC/Tracking/src/main/java/com/tracking/run/TrackingApplication.java
@@ -2,7 +2,14 @@ package com.tracking.run;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+
+@EntityScan(basePackages = {"com.tracking.model"})
+@ComponentScan(basePackages = {"com.tracking.*"})
+@EnableJpaRepositories(basePackages = {"com.tracking.repository"})
 @SpringBootApplication
 public class TrackingApplication {
 
