dcfee0285c06c4ace4ec39b2f8d47b0a0f2a4450
iLuchoGlober88 <luis.pineda@globant.com>
2022-03-23 20:34:14 -0500

    Final Thesis

---
+++ Topic9 - Final Tesis/src/main/java/com/hermes/msg/HermesMessengerApplication.java
@@ -0,0 +1,13 @@
+package com.hermes.msg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HermesMessengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HermesMessengerApplication.class, args);
+	}
+
+}
---
+++ Topic9 - Final Tesis/src/test/java/com/hermes/msg/HermesMessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hermes.msg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HermesMessengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
