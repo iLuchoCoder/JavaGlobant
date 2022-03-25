7a3506aa8d6d4ad3b00833c147d5f6576f02e08a
luis.pineda <luis.pineda@globant.com>
2022-03-24 23:33:14 -0500

    Final Thesis

--- _Final Thesis_/src/main/java/com/hermes/msg/model/Users.java
+++ _Final Thesis_/src/main/java/com/hermes/msg/model/Users.java
@@ -23,34 +23,34 @@ public class Users {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@Column(name="username",nullable = true)
+	@Column(name="username",nullable = false)
 	private String username;
 
-	@Column(name="name",nullable = true)
+	@Column(name="name",nullable = false)
 	private String name;
 
-	@Column(name="last_name",nullable = true)
+	@Column(name="last_name",nullable = false)
 	private String last_name;
 
-	@Column(name="identification_number",nullable = true)
+	@Column(name="identification_number",nullable = false)
 	private int identification_number;
 
-	@Column(name="address",nullable = true)
+	@Column(name="address",nullable = false)
 	private String address;
 
-	@Column(name="city",nullable = true)
+	@Column(name="city",nullable = false)
 	private String city;
 
-	@Column(name="state",nullable = true)
+	@Column(name="state",nullable = false)
 	private String state;
 
-	@Column(name="country",nullable = true)
+	@Column(name="country",nullable = false)
 	private String country;
 
-	@Column(name="zip_code",nullable = true)
+	@Column(name="zip_code",nullable = false)
 	private int zip_code;
 
-	@Column(name="password",nullable = true)
+	@Column(name="password",nullable = false)
 	private String password;
 
 	@ManyToMany(fetch = FetchType.EAGER,cascade = CascadeType.ALL)
