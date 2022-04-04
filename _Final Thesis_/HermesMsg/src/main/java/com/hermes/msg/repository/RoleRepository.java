995458e1d8ec3571dc3c349ae2a20b76ff815a48
iLuchoGlober88 <luis.pineda@globant.com>
2022-04-04 18:21:52 -0500

    Final Thesis

--- _Final Thesis_/HermesMsg/src/main/java/com/hermes/msg/repository/RoleRepository.java
+++ _Final Thesis_/HermesMsg/src/main/java/com/hermes/msg/repository/RoleRepository.java
@@ -1,12 +1,11 @@
 package com.hermes.msg.repository;
 
 import com.hermes.msg.model.Role;
-import com.hermes.msg.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface RoleRepository extends JpaRepository<Users,Long> {
+public interface RoleRepository extends JpaRepository<Role,Long> {
 
     public Optional<Role> findByName(String name);
 
