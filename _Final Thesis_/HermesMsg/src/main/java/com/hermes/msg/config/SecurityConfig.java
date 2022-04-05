b1b46892bc02b81a971e4b6bcef42968b99e678e
luis.pineda <luis.pineda@globant.com>
2022-04-04 21:45:45 -0500

    Final Thesis

--- _Final Thesis_/HermesMsg/src/main/java/com/hermes/msg/config/SecurityConfig.java
+++ _Final Thesis_/HermesMsg/src/main/java/com/hermes/msg/config/SecurityConfig.java
@@ -32,8 +32,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf()
                 .disable()
                 .authorizeRequests()
-                .antMatchers(HttpMethod.GET,"/api/***").permitAll()
-                .antMatchers("/api/hermes/auth/***").permitAll()
+                .antMatchers(HttpMethod.GET,"/api/**").permitAll()
+                .antMatchers("/api/hermes/auth/**").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -42,8 +42,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
+        auth.userDetailsService(customUserDetailService).passwordEncoder(passwordEncoder());
     }
+
 
     @Bean
     @Override
